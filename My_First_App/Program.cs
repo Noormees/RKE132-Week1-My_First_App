d25b52d0e9d9699369fcc6240668aa5a8bd58bbf
Noormees <garry.lobanoff@gmail.com>
2023-01-30 20:29:40 +0200

    create new app

---
+++ My_First_App/Program.cs
@@ -0,0 +1,18 @@
+﻿// This is only comment
+
+// Rakendus kysib nime
+// Tervitab nimepidi
+
+Console.WriteLine("Kirjuta ona nimi:");
+string UserName;
+UserName = Console.ReadLine();
+Console.WriteLine("Hello, " + UserName + "!");
+
+
+// string interpolation
+Console.WriteLine($"Hello, {UserName}!");
+
+
+
+
+
